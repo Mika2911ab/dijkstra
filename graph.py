6237904f527761eb3c90284879268d75856ef17c
Dominik Lankeit <dlankeit@uni-mainz.de>
2023-11-01 10:49:12 +0100

    ver. 0.0.0.0.1

---
+++ graph.py
@@ -0,0 +1,53 @@
+import math
+
+###nodes sind tupel (längengrad,breitengrad,Adjazensliste von ausgehenden Kanten)
+###edges sind tupel (from_node,to_node,abstand denk ich mal macht sinn)
+class PQueue:
+    def __init__(self):
+        self.items = []
+    def push(self,u,value):
+        1
+    def decrease_Key(self,u,value):
+        1
+    def pop_min(self):
+        1
+
+def Abstand(l1,b1,l2,b2):#ohne erde
+    return math.sqrt((l1-l2)**2+(b1-b2)**2)
+
+class Graph:
+    def __init__(self):
+        self.nodes = []
+        self.edges = []
+        self.nodevalues = []#kann auch in den tupel wie bei edge für dijkstra anpassen
+    def add_node(self,l,b):#nodes müssen in der reihnfolge geadded werden wie sie heißen
+        self.nodes += [(l,b,[])]
+        self.nodevalues += []
+    def add_edge(self,n1,n2):
+        (l1,b1, al1) = self.nodes[n1]
+        (l2, b2, al2) = self.nodes[n2]
+        dist = Abstand(l1,b1,l2,b2)
+        self.edges += [(n1,n2,dist)]
+        self.nodes[n1] = (l1,b1,al1+[(n1,n2,dist)])#hier vllt den index statt edge selbst
+        return (l1,b1,l2,b2)
+    def num_nodes(self):
+        return len(self.nodes)
+    def num_edges(self):
+        return len(self.edges)
+    def from_node(self,e):
+        r,_ = e
+        return r
+    def to_node(self,e):
+        _, r = e
+        return r
+    def out_edges(self,u):
+        _,_,a = self.nodes[u]
+        return a
+    def in_edges(self,u):
+        a = self.out_edges(u)
+        return [(n2,n1,dist) for (n1,n2,dist) in a]#weil sowieso immer beide existieren
+    def set_node_value(self,u,value):
+        self.nodevalues[u] = value
+    def node_value(self,u):
+        return self.nodevalues[u]
+
---
+++ main.py
@@ -0,0 +1,47 @@
+from graph import *
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+def ReadCSV(G,window):
+    with open('testnodes.csv')as file:
+        nodedict= {}
+        for line in file:
+            node,lg,bg = line.split(',')
+            nodedict[node] = len(nodedict)#gibt keine 2 nodes mit gleichen namen
+            G.add_node(float(lg),float(bg))
+            ###Knoten anzeigen
+            #btn = QPushButton(window)
+            #btn.setGeometry(*umrechnen(float(lg),float(bg)),10,10)  # Position und Größe des Buttons
+            #btn.setStyleSheet("background-color: red; border-radius: 5px;")  # Stilvorlage für den Kreis
+    #painter = QPainter(window)
+    #painter.setPen(QPen(QColor('black'),4.0, Qt.PenStyle.SolidLine))
+    with open('testedges.csv') as file:
+        for line in file:
+            n1,n2 = line.replace('\n','').split(',')
+            x1,y1,x2,y2 = G.add_edge(nodedict[n1],nodedict[n2])
+            #achtung jede edge a,b is auch b,a in der datei (anscheinend)
+            #nichtmal geschafft linie zu machen ):
+            #painter.drawLine(*umrechnen(x1, y1), *umrechnen(x2, y2))
+
+def umrechnen(lg,bg):
+    #[8.1611,8.3781]min/max längengrad
+    #[49.9468,50.1082]min/max breitengrad
+    #400 war länge und breite des fensters
+    #ohne erde
+    return (int(((lg-8.1611)/0.217)*400),int(((bg-49.9468)/0.1614)*400))
+def main():
+    G = Graph()
+    app = QApplication(sys.argv)
+    window = QWidget()
+    #window.setWindowTitle("Maps fake lol")
+    #window.setGeometry(100,100,400,400)
+    #painter = QPainter(window)
+    #painter.setPen(QColor.black())
+    #painter.drawLine(97,241,195,151)
+    ReadCSV(G,window)
+    #window.show()
+    #sys.exit(app.exec_())
+
+main()
